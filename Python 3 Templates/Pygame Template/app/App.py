22bef6fb516fc810cb29d3b0e2b3e137f9b2c7bd
generaldave <icepicktoeye@gmail.com>
2017-01-23 11:50:38 -0500

    Python 3 Pygame Template added.

---
+++ Python 3 Templates/Pygame Template/app/App.py
@@ -0,0 +1,60 @@
+########################################################################
+#                                                                      #
+# David Fuller                                                         #
+#                                                                      #
+# App class: App initializer                                           #
+#                                                                      #
+# Created on 2016-12-29                                                #
+#                                                                      #
+########################################################################
+
+########################################################################
+#                                                                      #
+#                          IMPORT STATEMENTS                           #
+#                                                                      #
+########################################################################
+
+from   .Constants import *   # Constants file
+import pygame                # For GUI
+
+########################################################################
+#                                                                      #
+#                               APP CLASS                              #
+#                                                                      #
+########################################################################
+
+class App(object):
+    def __init__(self, appDirectory: str) -> None:
+        self.appDirectory = appDirectory
+
+        # Set up GUI
+        self.setupGUI()
+
+        # Run app
+        self.runApp()
+
+    # Mehtod sets up GUI
+    def setupGUI(self) -> None:
+        # Screen attributes
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREEN_RESOLUTION)
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()      # For frames per second
+        self.mouse = pygame.mouse.get_pos()   # For mouse position
+
+    # Method runs app
+    def runApp(self) -> None:
+        running = True
+        while running:
+            for event in pygame.event.get():
+                
+                # Handle quit event
+                if event.type == pygame.QUIT:
+                    running = False            
+
+            # Update Screen
+            pygame.display.update()
+            self.clock.tick(FPS)            
+
+        # Close app cleanly
+        pygame.quit()
